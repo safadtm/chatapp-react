9494cebcec187e6d4d3d44e6eee2665a65544555
safadtm <safadtm2004@gmail.com>
2023-12-08 17:54:42 +0530

    v1

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {getAuth,GoogleAuthProvider} from "firebase/auth";
 import {getFirestore} from "firebase/firestore"
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCMGdGPE-fddMjyiRRnsw5X-98tzJ_6ozY",
   authDomain: "chatapp-react-73e89.firebaseapp.com",
