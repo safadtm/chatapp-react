3d821dd2a67aec77d7df8bf842c20060ebf04e9d
safadtm <safadtm2004@gmail.com>
2023-12-08 16:34:07 +0530

    cookie --created

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,10 +1,18 @@
 import { auth, provider } from "../firebase-config"
 import { signInWithPopup } from "firebase/auth"
 
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
 export const Auth = () => {
 
     const signInWithGoogle = async () => {
-        await signInWithPopup(auth, provider);
+     try {
+        const result = await signInWithPopup(auth, provider);
+      cookies.set("auth-token",result.user.refreshToken);
+     } catch (err) {
+         console.error(err);     
+     }
     };
 
     return (
