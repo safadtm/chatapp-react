09a78d830d771c084f777d8ab10904fd0e2e5c6f
safadtm <safadtm2004@gmail.com>
2023-12-08 16:29:10 +0530

    auth --created

---
+++ src/components/Auth.js
@@ -0,0 +1,16 @@
+import { auth, provider } from "../firebase-config"
+import { signInWithPopup } from "firebase/auth"
+
+export const Auth = () => {
+
+    const signInWithGoogle = async () => {
+        await signInWithPopup(auth, provider);
+    };
+
+    return (
+        <div className="auth">
+            <p>Sign In With Google To Continue</p>
+            <button onClick={signInWithGoogle}> Sign In With Google </button>
+        </div>
+    );
+};
