872c88864fd2a3b0c6d821530f01d7cee38d9a63
safadtm <safadtm2004@gmail.com>
2023-12-08 16:49:55 +0530

    auth --updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "./App.css";
 import { Auth } from "./components/Auth";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
-function App(){
+function App() {
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
+  const [room, setRoom] = useState(null);
+
+  const roomInputRef = useRef(null);
+
+  if (!isAuth) {
+    return (
+      <div>
+        <Auth setIsAuth={setIsAuth}/>
+      </div>
+    );
+  }
+
   return <div>
-    <Auth />
-  </div>
+    {room ?
+      (<div> Chat </div>) :
+
+      (
+        <div className="room">
+
+          <label>Enter Room Name:</label>
+          <input ref={roomInputRef} />
+          <button onClick={() => setRoom(roomInputRef.current.value)}>
+            Enter Chat
+          </button>
+        </div>
+      )
+    }
+  </div>;
+
 }
 export default App;
--- src/components/Auth.js
+++ src/components/Auth.js
@@ -4,15 +4,17 @@ import { signInWithPopup } from "firebase/auth"
 import Cookies from "universal-cookie";
 const cookies = new Cookies();
 
-export const Auth = () => {
+export const Auth = (props) => {
+    const { setIsAuth } = props;
 
     const signInWithGoogle = async () => {
-     try {
-        const result = await signInWithPopup(auth, provider);
-      cookies.set("auth-token",result.user.refreshToken);
-     } catch (err) {
-         console.error(err);     
-     }
+        try {
+            const result = await signInWithPopup(auth, provider);
+            cookies.set("auth-token", result.user.refreshToken);
+            setIsAuth(true);
+        } catch (err) {
+            console.error(err);
+        }
     };
 
     return (
